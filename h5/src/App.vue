22571e54d38da30b5efbc67b9b40b5cf87f25793
willpbn <will.li@pbn.com.cn>
2020-09-16 16:37:35 +0800

    basic vue-cordova application project

---
+++ h5/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <mt-button type="primary" @click="testMint">Mint-UI</mt-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods:{
+      testMint(){
+          this.$toast({
+              message: '操作成功',
+              iconClass: 'icon icon-success'
+          });
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
