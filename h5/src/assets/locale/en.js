6c3b802a7f12aecd8655856288addbb6e8a94c2e
DDSSLL <duqing@deviser.com.cn>
2021-01-30 16:54:59 +0800

    修改多语言中的英文替换

--- h5/src/assets/locale/en.js
+++ h5/src/assets/locale/en.js
@@ -1,8 +1,24 @@
 module.exports = {
-    basic:{
+    /*basic:{
         "main":"Main",
         "settings":"Settings",
         "login":"Login"
+    },*/
+    basic1080:{
+        "status":"状态",
+        "control":"控制",
+        "live":"直播",
+        "settings":"参数",
+        "me":"我的",
+        "login":"登录"
+    },
+    basic4000:{
+        "status":"状态",
+        "control":"控制",
+        "live":"接收机",
+        "settings":"参数",
+        "me":"我的",
+        "login":"登录"
     },
     tip:{
         "authSuccess":"Auth Success"
